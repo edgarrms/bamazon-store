c4331a29867c25a2e5d34ddd4ec7f7d61feed171
Edgar <ramosjair94@gmail.com>
2020-01-14 17:15:05 -0800

    added table

--- bam.js
+++ bam.js
@@ -21,5 +21,13 @@ var display = function () {
         console.log("----------------------------------");
         console.log("Below is your product");
     });
-
+    var table = new Table({
+        head: ["Product Id", "Product Description", "cost"],
+        colWidths: [12, 50, 8],
+        colAligns: ["center", "left", "right"],
+        style: {
+            head: ["aqua"],
+            compact: true
+        }
+    });
 }
