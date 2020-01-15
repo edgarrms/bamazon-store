281d80aac49e1d204ebbd730f11fd2c4761604ef
Edgar <ramosjair94@gmail.com>
2020-01-14 18:14:22 -0800

    queatity check added

--- bam.js
+++ bam.js
@@ -48,8 +48,43 @@ var shopping + function () {
     connection.query("SELECT * FROM products WERE Id=?", selection, function (err, res) {
         if (err) throw err;
         if (res.length === 0) {
-            console.log("No such product, use product Id from list above")
-        };
+            console.log("No such product, use product Id from list above");
+            shopping();
+        } else {
+            inquirer.prompt({
+                name: "quantity",
+                type: "input",
+                message: "How many items do you want to buy?"
+            }).then(function (answer2) {
+                var quantity = answer2.quantity;
+                if (quantity > res[0].stock_quantity) {
+                    console.log(
+                        "sorry we don't have that item, we do have" +
+                        res[0].stock_quantity +
+                        "of item selected"
+                    );
+                    shopping();
+                } else (
+                    console.log("");
+                console.log(res[0].products_name + "purchased");
+                console.log(quantity + "qty @ $" + res[0].price);
+
+                var newQuantity = res[0].stock_quantity + quantity;
+                connection.query(
+                    "UPDATE products SET stock_quantity = "
+                    newQuantity +
+                    " WHERE id = " + res[0].id,
+                    function (err, resUpdate) {
+                        if (err) throw err;
+                        console.log("thanks! order fullfilled");
+                        connection.end();
+                    }
+                );
+            )
+        })
+}
+        
+    
     })
 })
 
--- bamazonschema.sql
+++ bamazonschema.sql
@@ -4,10 +4,11 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products(
-    item_id INT(4) NOT NULL,
+    id INT(4) NOT NULL,
 	product_name VARCHAR(100) NOT NULL,
-	department_name VARCHAR(100) NOT NULL,
+	department_id VARCHAR(100) NOT NULL,
 	price DECIMAL(10,2) NOT NULL,
 	stock_quantity INT(20) NOT NULL,
-	PRIMARY KEY (item_id)
+	PRIMARY KEY (id),
+	KEY 'department_id' ('department_id')
     );
