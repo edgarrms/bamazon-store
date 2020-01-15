3e19831057356ce8e5d9485bfb1a2db9b81d1b59
Edgar <ramosjair94@gmail.com>
2020-01-14 17:05:35 -0800

    added display function to bamazon

--- bam.js
+++ bam.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,7 +11,15 @@ var connection = mysql.createConnection({
 
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    start();
-});
+connection.connect();
+
+var display = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("----------------------------------");
+        console.log("welcom to bamazon");
+        console.log("----------------------------------");
+        console.log("Below is your product");
+    });
+
+}
